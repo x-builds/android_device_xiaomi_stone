fbc164a0d6d21ff71af8a1815f27ac87da7b7f6a
cod999 <168747975+cod999@users.noreply.github.com>
2024-06-12 15:52:09 +0000

    Update and rename lineage_stone.mk to voltage_stone.mk

--- voltage_stone.mk
+++ voltage_stone.mk
@@ -9,17 +9,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Inherit from stone device
 $(call inherit-product, device/xiaomi/stone/device.mk)
 
-# Inherit some common crDroid stuff
-TARGET_SUPPORTS_QUICK_TAP  := true
-TARGET_SUPPORTS_CALL_RECORDING := true
-TARGET_INCLUDE_PIXEL_CHARGER := true
-
-PRODUCT_NAME := lineage_stone
+PRODUCT_NAME := voltage_stone
 PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
